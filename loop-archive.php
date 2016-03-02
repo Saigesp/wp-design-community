70ca8286846b628cc34736d27a3b6c439380ce3a
Santiago Espinosa <saigesp@gmail.com>
2016-03-02 22:12:29 +0100

    Fix loop archive styles based on flexbox

--- loop-archive.php
+++ loop-archive.php
@@ -1,12 +1,12 @@
-<section id="article-<?php the_ID(); ?>" class="wrap <?php if(has_post_thumbnail()) echo 'wrap--frame'; else echo 'wrap--content';?>">
+<section id="article-<?php the_ID(); ?>" class="wrap wrap--frame">
   <?php if(has_post_thumbnail()){?>
-    <figure class="thumb thumb--archive">
+    <figure class="thumb thumb--archive js-imagefill" style="height: 300px;">
       <a href="<?php the_permalink() ?>">
-        <?php the_post_thumbnail('medium');?>
+        <?php the_post_thumbnail('full');?>
       </a>
     </figure>
   <?php } ?>
-  <div class="content content--archive">
+  <div class="wrap wrap--content content content--archive">
     <h2 class="title title--archive" ><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
     <div class="description description--archive">
       <?php the_excerpt();?>

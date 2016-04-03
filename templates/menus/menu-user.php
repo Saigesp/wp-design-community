ef13fe7f78d63a6491827f50aeed742dba0f1fc2
Saigesp <saigesp@saigesp.es>
2016-04-03 12:02:58 +0200

    Set up presidence configuration page permissions

--- templates/menus/menu-user.php
+++ templates/menus/menu-user.php
@@ -30,59 +30,71 @@
       </li>
     <?php } ?>
 
-    <?php if(is_user_role('administrator')) { ?>
+    <?php if(is_user_role('administrator') || is_user_role('editor')) { ?>
       <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-admin">
         <a onclick="ToggleMenu('menuadmin')">Administración</a>
       </li>
     <?php } ?>
+
+    <li class="menu-item menu-item-type-custom menu-item-object-custom">
+      <a href="<?php echo get_bloginfo('url');?>/events">Eventos</a>
+    </li>
+
+    <li class="menu-item menu-item-type-custom menu-item-object-custom">
+      <a href="<?php echo get_bloginfo('url');?>/fee">Cuotas</a>
+    </li>
+
+    <li class="menu-item menu-item-type-custom menu-item-object-custom">
+      <a href="<?php echo get_bloginfo('url');?>/concursos">Concursos</a>
+    </li>
 
   </ul>
 <?php }?>
 </nav>
 
 
-<?php if(is_user_role('administrator')) { ?>
+<?php if(is_user_role('administrator') || is_user_role('editor')) { ?>
   <nav id="menuadmin" class="wrap wrap--menu wrap--menu__user js-menu animecubic350">
     <ul id="menu-admin-menu-base" class="menu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children">
 
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/configuration">Configuración de la página</a>
-      </li>
+      <?php if(is_user_role('administrator')) { ?>
+        <li class="menu-item menu-item-type-custom menu-item-object-custom">
+          <a href="<?php echo get_bloginfo('url');?>/configuration">Configuración de la página</a>
+        </li>
+      <?php } ?>
 
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/configuration-presidence">Presidencia</a>
-      </li>
+      <?php if(get_user_meta($current_user->ID, 'asociation_position', true) == 'presidente' || is_user_role('administrator')) { ?>
+        <li class="menu-item menu-item-type-custom menu-item-object-custom">
+          <a href="<?php echo get_bloginfo('url');?>/configuration-presidence">Presidencia</a>
+        </li>
+      <?php } ?>
 
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/configuration-secretary">Secretaría</a>
-      </li>
+      <?php if(get_user_meta($current_user->ID, 'asociation_position', true) == 'secretario' || is_user_role('administrator')) { ?>
+        <li class="menu-item menu-item-type-custom menu-item-object-custom">
+          <a href="<?php echo get_bloginfo('url');?>/configuration-secretary">Secretaría</a>
+        </li>
+      <?php } ?>
 
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/configuration-treasury">Tesorería</a>
-      </li>
+      <?php if(get_user_meta($current_user->ID, 'asociation_position', true) == 'tesorero' || is_user_role('administrator')) { ?>
+        <li class="menu-item menu-item-type-custom menu-item-object-custom">
+          <a href="<?php echo get_bloginfo('url');?>/configuration-treasury">Tesorería</a>
+        </li>
+      <?php } ?>
 
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/configuration-event">Gestión eventos</a>
-      </li>
+      <?php if(get_user_meta($current_user->ID, 'asociation_responsability', true) == 'rp_events' || is_user_role('administrator')) { ?>
+        <li class="menu-item menu-item-type-custom menu-item-object-custom">
+          <a href="<?php echo get_bloginfo('url');?>/configuration-event">Gestión eventos</a>
+        </li>
+      <?php } ?>
 
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/configuration-concursos">Gestión de concursos</a>
-      </li>
+      <?php if(get_user_meta($current_user->ID, 'asociation_responsability', true) == 'rp_concursos' || is_user_role('administrator')) { ?>
+        <li class="menu-item menu-item-type-custom menu-item-object-custom">
+          <a href="<?php echo get_bloginfo('url');?>/configuration-concursos">Gestión de concursos</a>
+        </li>
+      <?php } ?>
 
       <li class="menu-item menu-item-type-custom menu-item-object-custom">
         <a href="<?php echo get_bloginfo('url');?>/control-users">Control de usuarios</a>
-      </li>
-
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/events">Eventos</a>
-      </li>
-
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/fee">Cuotas</a>
-      </li>
-
-      <li class="menu-item menu-item-type-custom menu-item-object-custom">
-        <a href="<?php echo get_bloginfo('url');?>/concursos">Concursos</a>
       </li>
 
     </ul>

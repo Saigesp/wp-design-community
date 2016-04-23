f3d1fb491c840b42a97346562f520a1b03571e78
Saigesp <saigesp@saigesp.es>
2016-04-23 16:35:23 +0200

    structure home (top)

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 global $post;
 
 $args = array (
-	'post_type' => array(' event'),
+	'post_type' => array('event'),
   'posts_per_page' => 5
 );
 $query = new WP_Query( $args );
@@ -12,24 +12,24 @@ $query = new WP_Query( $args );
 
 <div class="flexboxer flexboxer--home">
 
-    <section class="wrap wrap--fullwidth wrap--harry">
-      <div class="wrap wrap--frame wrap--flex">
-        <div class="wrap wrap--frame wrap--frame__middle">
-          <div class="wrap wrap--logo">
-            <a href="<?php bloginfo('url'); ?>">
-              <?php if(wp_get_attachment_url(get_theme_mod( 'logo_file', true )) != ''){ ?>
-                <img alt="logo" src="<?php echo wp_get_attachment_url(get_theme_mod( 'logo_file', true )); ?>"/>
-              <?php } else { ?>
-                <img alt="logo" src="<?php echo get_template_directory_uri(); ?>/img/default/logo-white.png"/>
-              <?php } ?>
-            </a>
-          </div>
-        </div>
-        <div class="wrap wrap--frame wrap--frame__middle">
-            <p class="right">Otro menu</p>
+  <section class="wrap wrap--fullwidth wrap--harry">
+    <div class="wrap wrap--frame wrap--flex">
+      <div class="wrap wrap--frame wrap--frame__middle">
+        <div class="wrap wrap--logo">
+          <a href="<?php bloginfo('url'); ?>">
+            <?php if(wp_get_attachment_url(get_theme_mod( 'logo_file', true )) != ''){ ?>
+              <img alt="logo" src="<?php echo wp_get_attachment_url(get_theme_mod( 'logo_file', true )); ?>"/>
+            <?php } else { ?>
+              <img alt="logo" src="<?php echo get_template_directory_uri(); ?>/img/default/logo.png"/>
+            <?php } ?>
+          </a>
         </div>
       </div>
-    </section>
+      <div class="wrap wrap--frame wrap--frame__middle">
+          <p class="right">Otro menu</p>
+      </div>
+    </div>
+  </section>
 
   <?php if ($query->have_posts()) { ?>
     <section class="wrap wrap--fullwidth wrap--slider">
@@ -38,23 +38,39 @@ $query = new WP_Query( $args );
           <div class="gallery-cell">
             <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>" alt="" style="height:360px;" >
           </div>
-        <?php }
-        $query = new WP_Query( $args );
-        ?>
+        <?php } ?>
       </div>
     </section>
   <?php } ?>
 
+    <section class="wrap wrap--content wrap--transparent wrap--titlesection">
+      <h3>Acerca de AEDI</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint odio, veniam possimus et distinctio amet eos suscipit optio, nam nesciunt, facilis labore architecto assumenda minus. Delectus quaerat quibusdam consequuntur tempora?</p>
+    </section>
 
   <?php
+  $args = array (
+    'post_type' => array('event'),
+    'posts_per_page' => 5,
+    'offset' => 5
+  );
   $query = new WP_Query( $args );
-  if ($query->have_posts()) {
-      while ($query->have_posts()) { 
-        $query->the_post();
-        include( locate_template(  'templates/loops/loop-archive.php' ));
-      }
-  	}
+
+  if ($query->have_posts()) { 
+    $article_count = 0;
+    ?>
+
+    <section class="wrap wrap--content wrap--transparent wrap--titlesection">
+      <h3>Últimas actividades</h3>
+    </section>
+
+    <?php while ($query->have_posts()) { 
+      $query->the_post();
+      include( locate_template(  'templates/loops/loop-archive.php' ));
+    }
+	}
   ?>
+
 </div>
 
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
--- templates/loops/loop-archive.php
+++ templates/loops/loop-archive.php
@@ -1,6 +1,6 @@
-<section id="article-<?php the_ID(); ?>" class="wrap wrap--frame js-showonload js-showonload-active">
+<section id="article-<?php the_ID(); ?>" class="wrap wrap--frame wrap--article js-showonload js-showonload-active <?php if($article_count == 0) echo 'wrap--article__first';?>">
   <?php if(has_post_thumbnail()){?>
-    <figure class="thumb thumb--archive js-imagefill" style="height: 300px;">
+    <figure class="thumb thumb--archive js-imagefill">
       <a href="<?php the_permalink() ?>">
         <?php the_post_thumbnail('full');?>
       </a>
@@ -13,4 +13,5 @@
     </div>
   </div>
 </section>
+<?php $article_count++; ?>
 

e762b8d173034a8316ddbf0ea4499fcbd21b83ad
Saigesp <saigesp@gmail.com>
2016-07-16 06:27:34 +0200

    basic messages between users - beauty input

--- templates/sections/meeseeks.php
+++ templates/sections/meeseeks.php
@@ -43,40 +43,9 @@
     </div>
 
   <?php }elseif(is_author() ){ ?>
-    <?php if(get_current_user_id() == get_query_var('author')){ ?>
-      <div class="wrap wrap--frame">
-        <h4>Hola <?php echo wpdc_get_user_name(get_current_user_id());?></h4>
-        <ul class="list list--notification">
-        <?php
-        $notifications = array_reverse(get_user_notification_track(get_current_user_id()));
-        $index = count($track);
-        foreach ($notifications as $track) {
-          $output = '<li class="notification">';
-          if($track['type'] == 'message') {
-            $output .= '<span class="author">'.wpdc_get_user_name($track['changeby']).'</span>: ';
-            $output .= '<span class="msg">'.$track['msg'].'</span> ';
-          }
+    
+    <?php include(locate_template('templates/forms/form-messageuser.php')); ?>
 
-          //foreach ($track as $key => $value) $output .= $key.': '.$value.' | '; //Test purpouses
-
-          $output .= ' <span title="'.$track['date'].'" class="js-date-fromnow">'.$track['date'].'</span>';
-          $output .= '</li>';
-          echo $output;
-        }
-        ?>
-        </ul>
-      </div>
-    <?php }elseif(is_user_role('administrator') || is_user_role('editor') ){ ?>
-      <div class="wrap wrap--frame">
-        <h4><?php echo wpdc_get_user_name(get_query_var('author'));?></h4>
-        <form method="POST" action="">
-          <input type="text" name="msg" placeholder="Enviar mensaje a <?php echo wpdc_get_user_name(get_query_var('author'));?>"> 
-          <label for="sendmsg">Enviar</label>
-          <input type="submit" id="sendmsg" class="hidden" name="notification" value="message"></button>
-          <input type="hidden" name="action" value="update-notification"/>
-        </form>
-      </div>
-    <?php } ?>
   <?php }else{ ?>
 
 

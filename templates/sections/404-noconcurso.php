593f085fa877aab343462fe99df33d4b8305cc4a
Saigesp <saigesp@gmail.com>
2016-07-18 02:09:48 +0200

    initial page commit

--- templates/sections/404-noconcurso.php
+++
@@ -1,3 +0,0 @@
-<section class="wrap wrap--content wrap--transparent">
-	<h3 class="title title-section">No hay ningún concurso creado todavía</h3>
-</section>
--- templates/sections/404-nousers.php
+++
@@ -1,3 +0,0 @@
-<section class="wrap wrap--content wrap--transparent">
-	<h3 class="title title-section">No hay usuarios que mostrar :(</h3>
-</section>
